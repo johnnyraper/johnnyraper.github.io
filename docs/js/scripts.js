26a37ab99a3007f0763ba0bac54076af13962d5c
Johnny Raper <johnnyraper@googlemail.com>
2021-11-22 11:29:02 +0000

    feat(Website): Website update - November 2021

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,9 +1,34 @@
-$(window).scroll(function () {
-   var scroll = $(window).scrollTop();
+var fadeElements = document.getElementsByClassName("scrollFade");
 
-   if (scroll >= 500) {
-      $("nav").addClass("nav--scrolled");
-   } else {
-      $("nav").removeClass("nav--scrolled");
-   }
+function scrollFade() {
+  var viewportBottom = window.scrollY + window.innerHeight;
+
+  for (var index = 0; index < fadeElements.length; index++) {
+    var element = fadeElements[index];
+    var rect = element.getBoundingClientRect();
+
+    var elementFourth = rect.height / 4;
+    var fadeInPoint = window.innerHeight - elementFourth;
+    var fadeOutPoint = -(rect.height / 2);
+
+    if (rect.top <= fadeInPoint) {
+      element.classList.add("scrollFade--visible");
+      element.classList.add("scrollFade--animate");
+      element.classList.remove("scrollFade--hidden");
+    } else {
+      element.classList.remove("scrollFade--visible");
+      element.classList.add("scrollFade--hidden");
+    }
+
+    if (rect.top <= fadeOutPoint) {
+      element.classList.remove("scrollFade--visible");
+      element.classList.add("scrollFade--hidden");
+    }
+  }
+}
+
+document.addEventListener("scroll", scrollFade);
+window.addEventListener("resize", scrollFade);
+document.addEventListener("DOMContentLoaded", function () {
+  scrollFade();
 });
